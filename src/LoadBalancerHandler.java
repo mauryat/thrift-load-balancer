d59a046bccd598b9214142e1d8ab6b15d48ba71e
Maurya <user640378@users.noreply.github.com>
2015-06-14 14:52:12 -0400

    storing b.txt in reverse

--- src/LoadBalancerHandler.java
+++ src/LoadBalancerHandler.java
@@ -10,142 +10,138 @@ import shared.*;
 
 import java.io.*;
 
-public class LoadBalancerHandler implements LoadBalancer.Iface {
+public class LoadBalancerHandler implements LoadBalancer.Iface
+{
 
-  public void offLoad() {
-    System.out.println("offLoad()");
-    System.out.println(Server.fileName);
-    System.out.println(Server.portNum);
-    String str = tail2(new File(Server.fileName), 10);
-    System.out.println(str);
+  public void offLoad ()
+  {
+    System.out.println ("offLoad()");
+    System.out.println (Server.fileName);
+    System.out.println (Server.portNum);
+    String str = tail2 (new File (Server.fileName), 10);
+    System.out.println (str);
 
-    sendToBackupServer(str);
+    sendToBackupServer (str);
   }
 
-  public void load(String str) {
-    System.out.println("load()");
-    System.out.println(Server.fileName);
-    System.out.println(Server.portNum);
+  public void load (String str)
+  {
+    System.out.println ("load()");
+    System.out.println (Server.fileName);
+    System.out.println (Server.portNum);
 
-    System.out.println(str);
-    // prepend to b.txt
-    // write to new file
-    PrintWriter pout = null;
-    try {
-      pout = new PrintWriter("temp.txt");
-      pout.println(str);
-    } catch(IOException e) {
-      e.printStackTrace();
-    } finally {
-      pout.close();
+    System.out.println (str);
+
+    // append to b.txt
+    try
+    {
+      String dest = "b.txt";
+
+      FileWriter fstream = new FileWriter (dest, true);
+      BufferedWriter out = new BufferedWriter (fstream);
+
+      out.write (str);
+
+      out.close ();
+    }
+    catch (IOException e)
+    {
+      e.printStackTrace ();
     }
 
-    // append from b.txt to temp.txt and delete b.txt
-    try {
-		String source = Server.fileName;
-		String dest = "temp.txt";
- 
-		File fin = new File(source);
-		FileInputStream fis = new FileInputStream(fin);
-		BufferedReader in = new BufferedReader(new InputStreamReader(fis));
- 
-		FileWriter fstream = new FileWriter(dest, true);
-		BufferedWriter out = new BufferedWriter(fstream);
- 
-		String aLine = null;
-		while ((aLine = in.readLine()) != null) {
-			//Process each line and add output to Dest.txt file
-			out.write(aLine);
-			out.newLine();
-		}
-
-                // delete b.txt
-                fin.delete();
- 
-		// do not forget to close the buffer reader
-		in.close();
- 
-		// close buffer writer
-		out.close();
-
-                // rename temp.txt to b.txt
-                new File("temp.txt").renameTo(new File(Server.fileName)); 
-    } catch(IOException e) {
-      e.printStackTrace();
-    }
   }
 
   // client to server connection to invoke load() method
-  private void sendToBackupServer(String str) {
-    try {
+  private void sendToBackupServer (String str)
+  {
+    try
+    {
       TTransport transport;
-      transport = new TSocket("localhost", Server.peerPortNum);
-      transport.open();
+      transport = new TSocket ("localhost", Server.peerPortNum);
+      transport.open ();
 
-      TProtocol protocol = new  TBinaryProtocol(transport);
-      LoadBalancer.Client client = new LoadBalancer.Client(protocol);
+      TProtocol protocol = new TBinaryProtocol (transport);
+      LoadBalancer.Client client = new LoadBalancer.Client (protocol);
 
-      client.load(str);
+      client.load (str);
 
-      transport.close();
-    } catch (TException x) {
-      x.printStackTrace();
+      transport.close ();
+    } catch (TException x)
+    {
+      x.printStackTrace ();
     }
   }
- 
-  public SharedStruct getStruct(int key) {
-    System.out.println("getStruct(" + key + ")");
+
+  public SharedStruct getStruct (int key)
+  {
+    System.out.println ("getStruct(" + key + ")");
     return null;
   }
 
-public static String tail2( File file, int lines) {
+  public static String tail2 (File file, int lines)
+  {
     java.io.RandomAccessFile fileHandler = null;
-    try {
-        fileHandler = 
-            new java.io.RandomAccessFile( file, "rw" );
-        long fileLength = fileHandler.length() - 1;
-        StringBuilder sb = new StringBuilder();
-        int line = 0;
+    try
+    {
+      fileHandler = new java.io.RandomAccessFile (file, "rw");
+      long fileLength = fileHandler.length () - 1;
+      StringBuilder sb = new StringBuilder ();
+      int line = 0;
 
-        for(long filePointer = fileLength; filePointer != -1; filePointer--){
-            fileHandler.seek( filePointer );
-            int readByte = fileHandler.readByte();
+      for (long filePointer = fileLength; filePointer != -1; filePointer--)
+	{
+	  fileHandler.seek (filePointer);
+	  int readByte = fileHandler.readByte ();
 
-             if( readByte == 0xA ) {
-                if (filePointer < fileLength) {
-                    line = line + 1;
-                }
-            } else if( readByte == 0xD ) {
-                if (filePointer < fileLength-1) {
-                    line = line + 1;
-                }
-            }
-            if (line >= lines) {
-                break;
-            }
-            sb.append( ( char ) readByte );
-        }
+	  if (readByte == 0xA)
+	    {
+	      if (filePointer < fileLength)
+		{
+		  line = line + 1;
+		}
+	    }
+	  else if (readByte == 0xD)
+	    {
+	      if (filePointer < fileLength - 1)
+		{
+		  line = line + 1;
+		}
+	    }
+	  if (line >= lines)
+	    {
+	      break;
+	    }
+	  sb.append ((char) readByte);
+	}
 
-        String lastLine = sb.reverse().toString();
+      //String lastLine = sb.reverse().toString();
+      String str = sb.toString();
 
-        // truncate the file
-        fileHandler.setLength(fileLength - lastLine.length());
+      // truncate the file
+      fileHandler.setLength (fileLength - str.length());
 
-        return lastLine;
-    } catch( java.io.FileNotFoundException e ) {
-        e.printStackTrace();
-        return null;
-    } catch( java.io.IOException e ) {
-        e.printStackTrace();
-        return null;
+      return str;
     }
-    finally {
-        if (fileHandler != null )
-            try {
-                fileHandler.close();
-            } catch (IOException e) {
-            }
+    catch (java.io.FileNotFoundException e)
+    {
+      e.printStackTrace ();
+      return null;
     }
+    catch (java.io.IOException e)
+    {
+      e.printStackTrace ();
+      return null;
+    }
+    finally
+    {
+      if (fileHandler != null)
+	try
+	{
+	  fileHandler.close ();
+	}
+      catch (IOException e)
+      {
+      }
+    }
+  }
 }
-}
-
